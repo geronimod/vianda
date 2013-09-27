0085f6474084ded228a7114dfecba8b0491a3d3c
Geronimo Diaz <geronimo@gdiaz.(none)>
2013-09-27 12:39:46 -0300

    Force pg connection

--- db/config.rb
+++ db/config.rb
@@ -4,6 +4,7 @@ Dir["./db/models/*.rb"].each { |file| require file }
 
 # Establish the database connection; or, omit this and use the DATABASE_URL
 # environment variable as the connection string:
-set :database, (development? ? 'sqlite://vianda.db' : 'postgres://localhost/vianda')
+# set :database, (development? ? 'sqlite://vianda.db' : 'postgres://localhost/vianda')
+set :database, 'postgres://localhost/vianda'
 
 # Sequel::Model.plugin :json_serializer
