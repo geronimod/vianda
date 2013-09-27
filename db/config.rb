edd46f6d9576f25ebce3c3542a8a7b13cf1cb62d
Geronimo Diaz <geronimo@gdiaz.(none)>
2013-09-27 13:31:34 -0300

    Fix db connection

--- db/config.rb
+++ db/config.rb
@@ -1,10 +1,7 @@
+set :database, (development? ? 'sqlite://vianda.db' : 'postgres://localhost/vianda')
+
 require './db/migrations'
 require './db/callbacks'
 Dir["./db/models/*.rb"].each { |file| require file }
-
-# Establish the database connection; or, omit this and use the DATABASE_URL
-# environment variable as the connection string:
-# set :database, (development? ? 'sqlite://vianda.db' : 'postgres://localhost/vianda')
-set :database, 'postgres://localhost/vianda'
 
 # Sequel::Model.plugin :json_serializer
